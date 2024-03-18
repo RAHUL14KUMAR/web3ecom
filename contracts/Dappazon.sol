0db1e4ceb926fb83393ee16376f6ae1d98d6ceea
rahul14kumar <rahul.kumar.it25@heritageit.edu.in>
2024-03-18 13:14:10 +0530

    make add item iin the list and add test case

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -3,5 +3,36 @@ pragma solidity ^0.8.9;
 
 contract Dappazon {
     string public name="DAPPAZONE";
-    
+    address public owner;
+
+    struct Item{
+        uint id;
+        string name;
+        string category;
+        string image;
+        uint price;
+        uint256 rating;
+        uint256 stock;
+    }
+    mapping(uint256=>Item)public items;
+
+    constructor(){
+        owner=msg.sender;
+    }
+
+    // list the product
+    function listProduct(uint256 _id, string memory _name, string memory _category, string memory _image, uint _price, uint256 _rating, uint256 _stock) public {
+        require(msg.sender==owner, "Only owner can list the product");
+        Item memory item=Item(
+            _id,_name,_category,_image,_price,_rating,_stock
+        );
+
+        items[_id]=item;
+    }
+
+
+    // buy the product
+
+
+    // withdraw funds
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -5,13 +5,42 @@ const tokens = (n) => {
 
 describe("Dappazon", async() => {
   let dappazon;
-  before(async()=>{
+  let deployer,buyer;
+  let transaction;
+
+  beforeEach(async()=>{
+    // account setup
+    [deployer,buyer] = await ethers.getSigners()
+
+    // deploy contract
     const Dappazon = await ethers.getContractFactory("Dappazon")
     dappazon = await Dappazon.deploy()
     await dappazon.deployed()
+
+    // transaction of item
+    transaction=await dappazon.connect(deployer).listProduct(
+      1,
+      "shoes",
+      "clothing",
+      "image",
+      tokens(1),
+      4,
+      5
+    );
+  })
+
+  it("set the owner of contract",async()=>{
+    expect(await dappazon.owner()).to.equal(deployer.address);
   })
 
   it('has name', async() => {
     expect(await dappazon.name()).to.equal('DAPPAZONE')
+  })
+
+  it("should returns items attribute",async()=>{
+    await transaction.wait();
+
+    const item=await dappazon.items(1);
+    expect(item.id).to.equal(1);
   })
 })
